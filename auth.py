e0dac726469c429cd074f93398746e0a8e28f843
awerks <sustila24@gmail.com>
2025-03-09 21:42:45 +0100

    Refactor Google login to check for existing users by email or user ID and update session handling

--- auth.py
+++ auth.py
@@ -82,19 +82,15 @@ async def google_login(request: Request, db: AsyncSession = Depends(get_db)):
     photo_url = user.get("picture")
 
     # first email, if the user already registered with the same email
-    existing_user = await db.execute(
-        select(User.user_id).where(
-            User.user_id == user_id,
-        )
-    )
-    existing_user_id = existing_user.scalar_one_or_none()
+    existing_user = await db.execute(select(User.user_id).where(or_(User.email == email, User.user_id == user_id)))
+    existing_user_id = existing_user.scalar()
     if not existing_user_id:
         db.add(User(user_id=user_id, username=username, name=name, email=email))
         await db.commit()
 
     request.session.update(
         {
-            "user_id": user_id,
+            "user_id": existing_user_id or user_id,
             "first_name": name,
             "username": username,
             "photo_url": photo_url,
